f1b2df7cc870daf7de6ff243bac2f35d924b39da
Your Name <dinitayala@gmail.com>
2024-08-21 11:14:02 -0500

    Initial Commit

---
+++ modulo.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+
+def suggest_reactivation(data):
+    # Cargar y procesar datos históricos
+    df = pd.read_csv('/content/Honey 2013-2021.csv')
+
+    # Preparar características y objetivo
+    X = df[['month', 'temperature', 'rainfall']]
+    y = df['production']
+
+    # Entrenar modelo
+    model = RandomForestRegressor()
+    model.fit(X, y)
+
+    # Predecir mejor momento para reactivar
+    predictions = model.predict(data)
+    best_time = predictions.argmax()
+
+    return {'best_reactivation_time': best_time}
